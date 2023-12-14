1e56ed13c43f46d0553f37388f644bede9dae8fb
LeandroLicata <leandrolicata1@gmail.com>
2023-12-14 10:49:02 -0300

    card width

--- src/components/CocktailCard/styles.js
+++ src/components/CocktailCard/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const CardContainer = styled.div`
-  width: 300px; 
+  // width: 300px; 
   height: 375px; 
   border-radius: 1rem; 
   overflow: hidden;
